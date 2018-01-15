96c1f8babe7e34b6fa9e8560a2f1f3156ed16367
Jaco Kotzee <jaco@doodlesdigital.co.za>
2018-01-15 11:34:18 +0200

    refactoring

--- js/app.js
+++ js/app.js
@@ -1,18 +1,5 @@
-
 const students = document.getElementsByClassName('student-item');
 const paginationEl = document.getElementsByClassName('pagination')[0];
-// const button1 = document.getElementsByClassName('1');
-// const button2 = document.getElementsByClassName('2');
 
-// Show first 10 students
 showPage(1,students);
 appendPageLinks(students);
-
-// button1[0].addEventListener("click", function() {
-//     showPage(1,students);
-//     console.log("button 2 clicked");
-//   });
-// button2[0].addEventListener("click", function() {
-//     showPage(2,students);
-//     console.log("button 2 clicked");
-//   });
--- js/pagelinks.js
+++ js/pagelinks.js
@@ -2,29 +2,34 @@ const appendPageLinks = (studentList) => {
     // determine how many pages for this student list    
     const pageAmount = Math.ceil(studentList.length / 10);
     const page = document.querySelector('.page');
+    // create unordered list
     let ul = document.createElement('ul');
+    // append unordered list to .page div
     page.appendChild(ul);
+    // give unordered list the class "pagination"
     ul.className = 'pagination';
     // create a page link section
     // “for” every page
-    for(let i = 0; i < pageAmount; i++) {
+    for(let i = 1; i < pageAmount; i++) {
         // add a page link to the page link section
         let li = document.createElement('li');
         let link = document.createElement('a');
         link.setAttribute('href', '#')
         // add page number to link
-        link.innerHTML = (i + 1);
+        link.innerHTML = i;
         // append link to list element
         li.appendChild(link);
         // append list element to unordered list
         ul.appendChild(li);
     }
+    // select created links
     const buttons = document.querySelectorAll('.pagination li');
-    
-    for(let i = 0; i < buttons.length; i++) {
+    // loop through all the link elements 
+    for(let i = 1; i < buttons.length; i++) {
+        // assign eventlisteners to each link
         buttons[i].addEventListener("click", () => {
-            showPage((i + 1),students);  
-
+            // call showpage function using i as page number
+            showPage(i,students);  
             const active = document.querySelector('.pagination li a.active');
             if ( active ) {
                 active.className = '';
